5ed5eb1757f58d0097faa8c882486f9a13770144
JuanRodriguezAbarca <juanrabarca@gmail.com>
2015-02-20 22:31:58 +0100

    Remoteando

--- mavenJenkinsProject/src/test/java/mavenJenkinsProject/RunnerCabroner.java
+++ mavenJenkinsProject/src/test/java/mavenJenkinsProject/RunnerCabroner.java
@@ -7,12 +7,11 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class RunnerCabroner{
 	
-	WebDriver driver = new FirefoxDriver();
+	RemoteWebDriver driver;
 	
 	
 	@Before
